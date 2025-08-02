37f3fe07788ef86372c48df1e4562e9e06b64a0e
Zhao Zhan <zhaozhan.site@gmail.com>
2025-08-02 21:11:16 +0800

    chore: update blog configuration to change theme settings and modify matery theme options for visibility and functionality

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  MATERY_HOME_BANNER_ENABLE: true,
+  MATERY_HOME_BANNER_ENABLE: false,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   // MATERY_HOME_BANNER_GREETINGS: [
   //   'Hi，我是一个程序员',
@@ -15,21 +15,21 @@ const CONFIG = {
   MATERY_SHOW_START_READING: false,
 
   // 菜单配置
-  MATERY_MENU_CATEGORY: true, // 显示分类
-  MATERY_MENU_TAG: true, // 显示标签
-  MATERY_MENU_ARCHIVE: true, // 显示归档
+  MATERY_MENU_CATEGORY: false, // 显示分类
+  MATERY_MENU_TAG: false, // 显示标签
+  MATERY_MENU_ARCHIVE: false, // 显示归档
   MATERY_MENU_SEARCH: true, // 显示搜索
 
   MATERY_POST_LIST_COVER: true, // 文章封面
   MATERY_POST_LIST_SUMMARY: true, // 文章摘要
   MATERY_POST_LIST_PREVIEW: true, // 读取文章预览
 
-  MATERY_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  MATERY_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
+  MATERY_ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
+  MATERY_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
   MATERY_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
-  MATERY_ARTICLE_RECOMMEND: true, // 文章关联推荐
+  MATERY_ARTICLE_RECOMMEND: false, // 文章关联推荐
 
-  MATERY_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
+  MATERY_WIDGET_LATEST_POSTS: false, // 显示最新文章卡
   MATERY_WIDGET_ANALYTICS: false, // 显示统计卡
   MATERY_WIDGET_TO_TOP: true,
   MATERY_WIDGET_TO_COMMENT: true, // 跳到评论区
