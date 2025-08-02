56064d168fb9257cb74ef1a0087e9367c7a7e4ce
Zhao Zhan <zhaozhan.site@gmail.com>
2025-08-02 22:29:29 +0800

    fix: update homepage banner greeting and adjust post list cover hover settings in hexo theme configuration

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -1,12 +1,9 @@
 const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
-  // HEXO_HOME_BANNER_GREETINGS: [
-  //   'Hi，我是一个程序员',
-  //   'Hi，我是一个打工人',
-  //   'Hi，我是一个干饭人',
-  //   '欢迎来到我的博客🎉'
-  // ], // 首页大图标语文字
+  HEXO_HOME_BANNER_GREETINGS: [
+    '所有命运的馈赠，都已在暗中标好了价格',
+  ], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
@@ -23,7 +20,7 @@ const CONFIG = {
   HEXO_MENU_RANDOM: false, // 显示随机跳转按钮
 
   HEXO_POST_LIST_COVER: true, // 列表显示文章封面
-  HEXO_POST_LIST_COVER_HOVER_ENLARGE: true, // 列表鼠标悬停放大
+  HEXO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
 
   HEXO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   HEXO_POST_LIST_SUMMARY: true, // 文章摘要
